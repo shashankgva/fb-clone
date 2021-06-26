4808a7f2d61c911da4b62d387997610b1d39e619
shashank <shashanka@interactionone.com>
2021-06-26 13:47:52 +0530

    updates

--- src/Widgets.js
+++ src/Widgets.js
@@ -6,7 +6,7 @@ function Widgets() {
     <div className="widgets">
       <iframe
         src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2F20531316728%2Fposts%2F10154009990506729%2F&show_text=true&width=200&height=1500&appId"
-        width="200"
+        width="100%"
         height="100%"
         style={{ border: 'none', overflow: 'hidden' }}
         scrolling="no"
